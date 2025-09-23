5b8051765bd4ea8c0ffe0e432d9983b62c2ef43f
Iris Cheung <hokyinirischeung@gmail.com>
2025-09-23 00:28:33 -0700

    added comment into wave 1 #3(def add_to_watchlist())  and #4 (def watch_movie())

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -21,15 +21,17 @@ def create_movie(title, genre, rating):
 
 
 def add_to_watchlist(user_data, movie):
+    #located the watchlist in user_data and add movie into watchlist
     user_data["watchlist"].append(movie)
     return user_data
 
 def watch_movie(user_data, title):
+    #assign watchlist into a variable
     waiting_watch = user_data["watchlist"]
-    for movie in waiting_watch:
-        if title == movie["title"]:
-            user_data["watched"].append(movie)
-            waiting_watch.remove(movie)
+    for movie in waiting_watch: #loop through the waiting_watch list
+        if title == movie["title"]: # Compare if title is equal to the title in waiting_watch
+            user_data["watched"].append(movie) #If above condition is true then add the movie into the watched list in user_data
+            waiting_watch.remove(movie) #and remove the movie from the waiting_watch list
     return user_data
     
 
