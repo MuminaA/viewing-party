7de380fc1fe104496f40879eb0b548db02b284bb
Iris Cheung <hokyinirischeung@gmail.com>
2025-09-23 00:20:47 -0700

    Completed Wave 1 #3, #4, added some tests in test_wave_01

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,6 +1,7 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
+
     #print(rating)
     if not (title and genre and rating):
         #print('None')
@@ -13,6 +14,24 @@ def create_movie(title, genre, rating):
         }
         #print(movie_data)
         return movie_data
+    
+
+    
+
+
+
+def add_to_watchlist(user_data, movie):
+    user_data["watchlist"].append(movie)
+    return user_data
+
+def watch_movie(user_data, title):
+    waiting_watch = user_data["watchlist"]
+    for movie in waiting_watch:
+        if title == movie["title"]:
+            user_data["watched"].append(movie)
+            waiting_watch.remove(movie)
+    return user_data
+    
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
@@ -32,4 +51,4 @@ def create_movie(title, genre, rating):
 # ------------- WAVE 5 --------------------
 # -----------------------------------------
 
-create_movie("It Came from the Stack Trace", "Horror", 3.1)
+# create_movie("It Came from the Stack Trace", "Horror", 3.1)
